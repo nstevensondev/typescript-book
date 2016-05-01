6911f8525aa9079c65408e281c27f414aa551d46
nstevensondev <nelson.stevenson@gmail.com>
2016-05-01 02:38:49 -0300

    Cambios 01-05-2016

--- js/test.js
+++ js/test.js
@@ -1,1 +1,11 @@
-var t = 1;
+var Person = (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.greet = function () {
+        alert("Hi! My name is " + this.name);
+    };
+    return Person;
+}());
+var remo = new Person("Remo");
+remo.greet(); // "Hi! My name is Remo"
--- ts/test.ts
+++ ts/test.ts
@@ -1,1 +1,14 @@
-var t : number = 1;
+///<reference path="../typings/handlebars/handlebars.d.ts" />
+///<reference path="../typings/jquery/jquery.d.ts" />
+
+class Person {
+	private name : string;
+	constructor(name : string) {
+		this.name = name;
+	}
+	greet() : void {
+		alert(`Hi! My name is ${this.name}`);
+	}
+}
+var remo = new Person("Remo");
+remo.greet(); // "Hi! My name is Remo"
